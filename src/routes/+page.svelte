e6b7e705f93c73d4de7094aa4b9f957ce45f71fd
Davide Tello <tello.davide@gmail.com>
2022-10-09 17:19:52 +0200

    Add main image and header text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import rei from '$lib/images/rei.jpg';
+</script>
+
+<main class="container mx-auto px-4 py-8">
+	<img src={rei} alt="rei" class="rounded-full h-32 w-32 mx-auto" />
+
+	<div class="flex flex-col items-center font-light">
+		<h1 class="text-4xl mt-8 font-bold mb-4">Lil' Rei</h1>
+		<h2>Her Royal Potatoness 👑</h2>
+		<h2>Shiba Inufluencer based in Berlin</h2>
+	</div>
+</main>
